c3b1b71c07599e6e16cdac22f069c1fdff6a818b
cranialnut <n.yodrabum@gmail.com>
2020-04-23 23:41:59 +0700

    2

--- assignments/28.py
+++ assignments/28.py
@@ -1,3 +1,12 @@
-x = input()
-print(x)
-
+n1 = int(input("กรุณากรอกเลขจำนวนที่หนึ่ง :"))
+n2 = int(input("กรุณากรอกเลขจำนวนที่สอง :"))
+strF = str(n1)
+strS = str(n2)
+sum = n1 + n2
+dif = n1 - n2
+mul = n1 * n2
+mod = n1 / n2
+print(strF,"+", strS,"=",sum)
+print(strF,"-", strS,"=",dif)
+print(strF,"*", strS,"=",mul)
+print(strF,"/", strS,"=",mod)
--- assignments/29.py
+++ assignments/29.py
@@ -1,7 +1,16 @@
-price = int(input("ราคาต้นทุน(บาท):"))
-vat = 7
-result = price + (price*7/100)
-print("ราคารวมภาษีมูลค่าเพิ่ม:",result,"บาท")
+n1 = int(input("กรุณากรอกเลขจำนวนที่หนึ่ง :"))
+n2 = int(input("กรุณากรอกเลขจำนวนที่สอง :"))
+strF = str(n1)
+strS = str(n2)
+sum = n1 + n2
+dif = n1 - n2
+mul = n1 * n2
+mod = n1 / n2
+print(strF,"+", strS,"=",sum)
+print(strF,"-", strS,"=",dif)
+print(strF,"*", strS,"=",mul)
+print(strF,"/", strS,"=",mod)
+
 
 
 
