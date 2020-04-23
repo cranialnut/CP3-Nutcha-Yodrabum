fdd658bbdbffe9c03d8aa7f184808129a6ec6485
cranialnut <n.yodrabum@gmail.com>
2020-04-23 22:56:22 +0700

    23.4

---
+++ assignments/29.py
@@ -0,0 +1,8 @@
+price = int(input("ราคาต้นทุน(บาท):"))
+vat = 7
+result = price + (price*7/100)
+print("ราคารวมภาษีมูลค่าเพิ่ม:",result,"บาท")
+
+
+
+
---
+++ assignments/Exercise5_1_Nutcha_Y.py
@@ -0,0 +1,10 @@
+firstNumber = int(input("กรุณากรอกเลขจำนวนที่หนึ่ง :"))
+secondNumber = int(input("กรุณากรอกเลขจำนวนที่สอง :"))
+sum = firstNumber + secondNumber
+dif = firstNumber - secondNumber
+mul = firstNumber * secondNumber
+div = firstNumber / secondNumber
+print("ผลบวกของจำนวนที่หนึ่งและจำนวนที่สองมีค่าเท่ากับ",sum)
+print("ผลลบของจำนวนที่หนึ่งและจำนวนที่สองมีค่าเท่ากับ ",dif)
+print("ผลคูณของจำนวนที่หนึ่งและจำนวนที่สองมีค่าเท่ากับ",mul)
+print("ผลหารของจำนวนที่หนึ่งและจำนวนที่สองมีค่าเท่ากับ",div)
